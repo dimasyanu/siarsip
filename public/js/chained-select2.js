5c051243457bc291f05925f5a5da92486f365877
yanoo <dimasyanu@yahoo.co.id>
2017-09-28 08:11:56 +0700

    change print methode

--- public/js/chained-select2.js
+++ public/js/chained-select2.js
@@ -1,68 +1,61 @@
-$(document).ready(function() {
-		$('.select2').select2();
+function chainSelect2(el) {
+	var chain = el.data('chain');
 
-		if($('#select_room').val() == 0){
-			checkSelect($('#select_room').val(), $('#select_shelf'));
-		}
+	if(el.val() == 0) checkSelect(el.val(), $(chain));
 
-		$('#select_room').on("select2:select", function(e) {
-			checkSelect($(this).val(), $('#select_shelf'));
-		});
+	el.on("select2:select", function(e) {
+		checkSelect($(this).val(), $(chain));
+	});
 
-		$('#select_shelf').on("select2:select", function(e) {
-			checkSelect($(this).val(), $('#select_box'));
-		});
+	if($(chain).attr('data-chain'))
+		chainSelect2($(chain));
+}
 
-		$('#select_box').on("select2:select", function(e) {
-			checkSelect($(this).val(), $('#select_section'));
-		});
-
-		function checkSelect(id, el) {
-			var oldVal = el.val();
-			el.empty();
-			if(id == 0){
-				if(el.data('chain')) checkSelect(0, $(el.data('chain')));
+function checkSelect(id, el) {
+	var oldVal = el.val();
+	el.empty();
+	if(id == 0){
+		if(el.data('chain')) checkSelect(0, $(el.data('chain')));
+		el.prop('disabled', true);
+	}
+	else{
+		$.ajax({
+			url      : root + '/api/' + el.data('alias') + '/' + id,
+			type     : 'GET',
+			dataType : 'json',
+			data     : {}
+		})
+		.done(function(results) {
+			if(results.length > 0) {
+				var model = el.parent('div').prev('label').text();
+				el.append($('<option>', {
+					value: "0",
+					text: 'Pilih ' + model + '..'
+				}));
+				for(var i = 0; i < results.length; i++){
+					el.append($('<option>', {
+						value: results[i].id,
+						text: results[i].name
+					}));
+				}
+				el.prop('disabled', false);
+				el.trigger('change').val(oldVal);
+				if(el.data('chain')){
+					checkSelect(0, $(el.data('chain')));
+					el.unbind("select2:select").on("select2:select", function() {
+						checkSelect(el.val(), $(el.data('chain')));
+					});
+				}
+			}
+			else {
+				if(el.data('chain')){
+					checkSelect(0, $(el.data('chain')));
+				}
 				el.prop('disabled', true);
 			}
-			else{
-				$.ajax({
-					url      : root + '/api/' + el.data('alias') + '/' + id,
-					type     : 'GET',
-					dataType : 'json',
-					data     : {}
-				})
-				.done(function(results) {
-					if(results.length > 0) {
-						var model = el.parent('div').prev('label').text();
-						el.append($('<option>', {
-							value: "0",
-							text: 'Pilih ' + model + '..'
-						}));
-						for(var i = 0; i < results.length; i++){
-							el.append($('<option>', {
-								value: results[i].id,
-								text: results[i].name
-							}));
-						}
-						el.prop('disabled', false);
-						el.trigger('change').val(oldVal);
-						if(el.data('chain')){
-							checkSelect(0, $(el.data('chain')));
-							el.unbind("select2:select").on("select2:select", function() {
-								checkSelect(el.val(), $(el.data('chain')));
-							});
-						}
-					}
-					else {
-						if(el.data('chain')){
-							checkSelect(0, $(el.data('chain')));
-						}
-						el.prop('disabled', true);
-					}
-				})
-				.fail(function() {
-					console.log("Ajax error");
-				});
-			}
-		}
-	});
+		})
+		.fail(function() {
+			console.log("Ajax error");
+		});
+	}
+}
